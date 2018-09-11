fc86f6608e248395d038d21fa69666d7a60a14b2
Richard Mable <richardbmable@gmail.com>
2018-09-11 13:56:26 -0600

    added an option input to select which mode of the program can be run with specific features turned on or off

--- main.go
+++ main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
 func main() {
 	fmt.Println("Program started...")
-	// infinite loop to take inifinite entries, don't have to restart each time
-	for {
-		pwd := hashPassword(passwordCLineEntry())
-		fmt.Println("Your base64 encoding password:")
-		fmt.Println(pwd)
+	var programOption int64
+	fmt.Println("Select a program option by entering a number:\n 1: Command line input to return SHA512 Base64 encoded hash\n 2: Hash and encode passwords over HTTP")
+	_, err := fmt.Scan(&programOption)
+	if err != nil {
+		log.Println(err)
+	}
+	if programOption == 1 {
+		fmt.Println("Program 1, command line input started")
+		// infinite loop to take inifinite entries, don't have to restart each time
+		for {
+			pwd := hashPassword(passwordCLineEntry())
+			fmt.Println("Your base64 encoded password:")
+			fmt.Println(pwd)
+		}
+	} else if programOption == 2 {
+		fmt.Println("Program 2, http mode started")
+		//http requests
 	}
 }
